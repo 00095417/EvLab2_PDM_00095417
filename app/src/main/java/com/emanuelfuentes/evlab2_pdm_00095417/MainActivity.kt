3905782890f78ea5b0d6ca6f722337fc13c7fc75
Carlos Ruiz <00095417@uca.edu.sv>
2019-05-03 08:11:10 -0600

    trabajando los fragmentos

--- app/src/main/java/com/emanuelfuentes/evlab2_pdm_00095417/MainActivity.kt
+++ app/src/main/java/com/emanuelfuentes/evlab2_pdm_00095417/MainActivity.kt
@@ -3,10 +3,41 @@ package com.emanuelfuentes.evlab2_pdm_00095417
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StaticFragment.OnSelectOption {
+
+    override fun onAction(id: Int) {
+        var content = when (id) {
+            1 -> {
+                "Hola"
+            }
+            2 -> {
+                "Wiii"
+            }
+            else -> {
+                "Fuck!"
+            }
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_content, ContentFragment.newInstance(content))
+            .addToBackStack("Co")
+            .commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var barFragment = StaticFragment.newInstace(
+            "Previous",
+            "Forward"
+        )
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_bar, barFragment)
+                .commit()
+        }
     }
 }
